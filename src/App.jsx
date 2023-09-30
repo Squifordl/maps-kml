d4270beb54070329d950af59e6bf53deff6b31dd
Squiford <edisonfranco2566@gmail.com>
2023-09-30 13:44:29 -0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useRef, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const mapRef = useRef(null);
+  const searchBoxRef = useRef(null);
+  const scriptURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDslUlwET6q743dgoKMa2BD-gfpIF_4vUo&libraries=places&callback=initMap`;
+
+  useEffect(() => {
+    window.initMap = function () {
+      const map = new window.google.maps.Map(mapRef.current, {
+        zoom: 11,
+        center: { lat: -30.0346, lng: -51.2177 },
+        mapTypeId: 'satellite'
+      });
+      const kmlUrls = [
+        'https://raw.githubusercontent.com/Squifordl/kml/main/PORTO_ALEGRE.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/ESTEIO.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/SAPUCAIA_DO_SUL.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/AT_TOTAL.kmz.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/NOVO_HAMBURGO.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/Lugares%20tempor%C3%A1rios%20(1).kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/Lugares%20tempor%C3%A1rios.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/SAO%20LEOPOLDO.kml',
+        'https://raw.githubusercontent.com/Squifordl/kml/main/SAPUCAIA_DO_SUL.kml',
+      ];
+
+      kmlUrls.forEach(url => {
+        const kmlLayer = new window.google.maps.KmlLayer({
+          url: url,
+          map: map,
+        });
+
+        window.google.maps.event.addListener(kmlLayer, 'status_changed', function () {
+          console.log(`Status do KML (${url}):`, kmlLayer.getStatus());
+        });
+      });
+
+      const mapTypeControlOptions = {
+        style: window.google.maps.MapTypeControlStyle.BOTTOM_CENTER,
+        position: window.google.maps.ControlPosition.HORIZONTAL_BAR,
+      };
+      map.setOptions({ mapTypeControlOptions });
+
+      const trafficLayer = new window.google.maps.TrafficLayer();
+      trafficLayer.setMap(map);
+
+      const searchBox = new window.google.maps.places.SearchBox(searchBoxRef.current);
+      map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(searchBoxRef.current);
+
+      searchBox.addListener('places_changed', () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length === 0) {
+          return;
+        }
+
+        const bounds = new window.google.maps.LatLngBounds();
+        places.forEach(place => {
+          if (!place.geometry) return;
+          bounds.extend(place.geometry.location);
+        });
+        map.fitBounds(bounds);
+      });
+    };
+
+    if (!document.querySelector(`script[src="${scriptURL}"]`)) {
+      const script = document.createElement('script');
+      script.src = scriptURL;
+      script.defer = true;
+      script.async = true;
+      document.head.appendChild(script);
+    }
+  }, [scriptURL]);
+
+  return (
+    <div className="App">
+      <input ref={searchBoxRef} className="search-bar" type="text" placeholder="Pesquisar..." />
+      <div className="map-container" ref={mapRef}></div>
+    </div>
+  );
+}
+
+export default App;
