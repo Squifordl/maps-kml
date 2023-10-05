6da06e183348ddcdc6e9db6c268853baf7bdecf4
Squiford <edison.franco2504@gmail.com>
2023-10-04 22:46:30 -0300

    AA

---
+++ src/utils/arrayKML.js
@@ -0,0 +1,13 @@
+const kmlUrls = [
+    'https://raw.githubusercontent.com/Squifordl/kml/main/PORTO_ALEGRE.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/ESTEIO.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/SAPUCAIA_DO_SUL.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/AT_TOTAL.kmz.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/NOVO_HAMBURGO.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/Lugares%20tempor%C3%A1rios%20(1).kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/Lugares%20tempor%C3%A1rios.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/SAO%20LEOPOLDO.kml',
+    'https://raw.githubusercontent.com/Squifordl/kml/main/SAPUCAIA_DO_SUL.kml',
+];
+
+export default kmlUrls;
