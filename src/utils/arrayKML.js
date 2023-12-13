abffd81ea9dfb62c518c61ba2b56fe5ca3462dfb
Squiford <edisonfranco2566@gmail.com>
2023-12-13 12:29:18 -0300

    Update arrayKML.js

--- src/utils/arrayKML.js
+++ src/utils/arrayKML.js
@@ -1,23 +1,30 @@
-const axios = require('axios');
+const Octokit = require('octokit');
 
 const githubToken = process.env.TOKEN;
 const repoOwner = 'squifordl';
-const repoName = 'kml';
-const kmlFolderPath = 'folder';
 
 async function getKmlUrls() {
     try {
-        const response = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${kmlFolderPath}`, {
+        const octokit = new Octokit({
+            auth: githubToken
+          })
+          
+          const tt = await octokit.request('GET /repos/{owner}/{repo}', {
+            owner: repoOwner,
+            repo: 'kml',
             headers: {
-                'Authorization': `token ${githubToken}`
+              'X-GitHub-Api-Version': '2022-11-28'
             }
-        });
+          })
 
-        const kmlUrls = response.data
-            .filter(file => file.name.endsWith('.kml') || file.name.endsWith('.kmz'))
-            .map(file => file.html_url);
+          console.log(tt)
+        // const kmlUrls = response.data
+        //     .filter(file => file.name.endsWith('.kml') || file.name.endsWith('.kmz'))
+        //     .map(file => file.html_url);
 
-        return kmlUrls;
+        
+
+        // return kmlUrls;
     } catch (error) {
         console.error("Erro ao acessar o repositório:", error);
         return [];
