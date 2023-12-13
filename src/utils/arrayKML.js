83f7f2237c897bb6574e1683a94cb50e266aef43
Squiford <edisonfranco2566@gmail.com>
2023-12-13 12:18:43 -0300

    Update arrayKML.js

--- src/utils/arrayKML.js
+++ src/utils/arrayKML.js
@@ -3,17 +3,18 @@ const axios = require('axios');
 const githubToken = process.env.TOKEN;
 const repoOwner = 'squifordl';
 const repoName = 'kml';
+const kmlFolderPath = 'folder';
 
-axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}`, {
+axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${kmlFolderPath}`, {
     headers: {
         'Authorization': `token ${githubToken}`
     }
 }).then(response => {
-    const kmlUrls = response
+    const kmlUrls = response.data
         .filter(file => file.name.endsWith('.kml') || file.name.endsWith('.kmz'))
+        .map(file => file.html_url);
 
     console.log(kmlUrls);
 }).catch(error => {
     console.error("Erro ao acessar o repositório:", error);
 });
-
