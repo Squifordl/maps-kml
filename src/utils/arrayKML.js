591dc63719b8ec69a91ec9320722daad755d9893
Squiford <edisonfranco2566@gmail.com>
2023-12-13 12:34:24 -0300

    Update arrayKML.js

--- src/utils/arrayKML.js
+++ src/utils/arrayKML.js
@@ -1,30 +1,26 @@
-const Octokit = require('octokit');
+const request = require('request')
 
 const githubToken = process.env.TOKEN;
-const repoOwner = 'squifordl';
 
 async function getKmlUrls() {
     try {
-        const octokit = new Octokit({
-            auth: githubToken
-          })
-          
-          const tt = await octokit.request('GET /repos/{owner}/{repo}', {
-            owner: repoOwner,
-            repo: 'kml',
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28'
-            }
-          })
-
-          console.log(tt)
-        // const kmlUrls = response.data
-        //     .filter(file => file.name.endsWith('.kml') || file.name.endsWith('.kmz'))
-        //     .map(file => file.html_url);
-
+        const URL = 'https://github.com/Squifordl/kml/tree/main/folder';
         
+        var options = {
+          url: URL,
+          headers: {
+            'Authorization': 'token ' + githubToken
+          }
+        };
 
-        // return kmlUrls;
+        function callback(error, response, body) {
+            console.log(response.statusCode);
+            console.error(error);
+            console.log(body);
+      }
+      
+      request(options, callback);
+      
     } catch (error) {
         console.error("Erro ao acessar o repositório:", error);
         return [];
