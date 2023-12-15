9e48926eada30941c64854ae1d697425e61e32e5
Squiford <edisonfranco2566@gmail.com>
2023-12-15 12:40:13 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MapComponent from './pages/Map';
+import DiscontinuedPopup from './pages/Pop';
 
 function App() {
   return (
     <div className="App">
       <MapComponent />
+      <DiscontinuedPopup />
     </div>
   );
 }
---
+++ src/pages/Pop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Pop.css';
+
+function DiscontinuedPopup() {
+  return (
+    <div className="popup-container">
+      <div className="popup-content">
+        <p>O site foi descontinuado.</p>
+      </div>
+    </div>
+  );
+}
+
+export default DiscontinuedPopup;
