f782c1fb33570ac2e0868ee84d46a7b173a754de
Squiford <edisonfranco2566@gmail.com>
2023-12-18 15:09:00 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import MapComponent from './pages/Map';
-import DiscontinuedPopup from './pages/Pop';
 
 function App() {
   return (
     <div className="App">
       <MapComponent />
-      <DiscontinuedPopup />
     </div>
   );
 }
